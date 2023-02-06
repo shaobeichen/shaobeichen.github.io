793aec2f3079649f65a399319f29989ec095f48e
shaobeichen <shaobeichen@outlook.com>
2023-02-06 21:14:21 +0800

    chore: 更新路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: import.meta.env.BASE_URL,
   scrollBehavior() {
     return { left: 0, top: 0 }
