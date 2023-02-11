7eb6adeaa39fb78a6a5886d8d2cc0abe514f33a7
shaobeichen <shaobeichen@outlook.com>
2023-02-11 17:12:21 +0800

    chore: 更新页面路由

--- src/utils/mocks/home.js
+++ src/utils/mocks/home.js
@@ -1,26 +1,31 @@
 export default [
   {
-    link: '/v-model',
+    link: '/nocode',
+    chapter: '第六期',
+    title: 'chatGPT优化代码效果怎么样？',
+  },
+  {
+    link: '/p5-vmodel',
     chapter: '第五期',
     title: 'v-model在组件上怎么使用？',
   },
   {
-    link: '/onbeforeunload',
+    link: '/p4-onbeforeunload',
     chapter: '第四期',
     title: 'Vue如何实现退出前保存？',
   },
   {
-    link: '/filter',
+    link: '/p3-filter',
     chapter: '第三期',
     title: '如何优雅的实现价格千位添加逗号？',
   },
   {
-    link: '/toast',
+    link: '/p1-toast',
     chapter: '第二期',
     title: '如何实现一个命令式toast组件？',
   },
   {
-    link: '/toast',
+    link: '/p1-toast',
     chapter: '第一期',
     title: '如何2分钟写一个toast组件？',
   },
---
+++ src/views/nocode.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nocode">
+    <div>无代码演示</div>
+  </div>
+</template>
+
+<style scoped>
+.nocode {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+}
+</style>
