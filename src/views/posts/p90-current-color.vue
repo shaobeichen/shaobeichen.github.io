f81c3a4d39f0cef305d5996d6c91c6c1eeb6d6d3
shaobeichen <shaobeichen@outlook.com>
2024-08-19 23:44:40 +0800

    feat: current-color

---
+++ src/views/posts/p90-current-color.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <button class="button">提交</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  .button {
+    color: red;
+    box-shadow: 0 0 10px 10px currentColor;
+    width: 400px;
+    height: 100px;
+    margin: 0 auto;
+    background: transparent;
+    font-size: 24px;
+    font-weight: bold;
+  }
+}
+</style>
